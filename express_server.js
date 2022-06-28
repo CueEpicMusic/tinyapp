f11ac458ef924c2fa7e918f2c0f227e544a8ecdf
CueEpicMusic <chiatunglo@hotmail.com>
2022-06-28 01:07:47 +0000

    add post method, redirect page

--- express_server.js
+++ express_server.js
@@ -1,45 +1,35 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
 const bodyParser = require("body-parser");
+
+//Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//View engine
 app.set("view engine", "ejs");
 
-function generateRandomString() {
+const generateRandomString = () => {
   let r = (Math.random().toString(36).substring(2,8));
-  return(r);
+  return r;
 }
 
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+//Get
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
 });
 
 app.get("/urls", (req, res) => {
@@ -59,7 +49,16 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL
+  const longURL = urlDatabase[shortURL]
+  res.redirect(longURL);
+});
+
+//Post
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString()
+  const longURL = req.body.longURL
+  urlDatabase[shortURL] = longURL
+  res.redirect(`/urls/${shortURL}`); // Respond with 'Ok' (we will replace this)
 });
